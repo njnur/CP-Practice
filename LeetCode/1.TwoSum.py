df19fc9312584a125883af6588bf803cf20e337b
Nur <mnjnurrumen@gmail.com>
2022-01-18 18:00:22 +0600

    Add bruteforce method for twosum problem

--- LeetCode/1.TwoSum.py
+++ LeetCode/1.TwoSum.py
@@ -4,6 +4,14 @@ from typing import List, Any
 class Solution:
     @staticmethod
     def two_sum_bruteforce(nums: List[int], target: int) -> Any:
+        for num_1_index in range(0, len(nums)):
+            for num_2_index in range(1, len(nums)):
+                if target == nums[num_1_index] + nums[num_2_index]:
+                    return [num_1_index, num_2_index]
+        return None
+
+    @staticmethod
+    def two_sum_pointer_technique(nums: List[int], target: int) -> Any:
         for index_1 in range(0, len(nums)):
             num_to_find = target - nums[index_1]
             for index_2 in range(index_1+1, len(nums)):
@@ -26,6 +34,6 @@ class Solution:
 
 if __name__ == '__main__':
     print(Solution().two_sum_bruteforce(
-        nums=[3, 2, 3],
-        target=6
+        nums=[2, 7, 11, 15],
+        target=9
     ))
