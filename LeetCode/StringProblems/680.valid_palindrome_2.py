af276596718379c1f07a56e5d09ff2e582bff6a8
Nur <mnjnurrumen@gmail.com>
2022-10-05 18:12:16 +0600

    remove init.py files

---
+++ LeetCode/StringProblems/680.valid_palindrome_2.py
@@ -0,0 +1,11 @@
+class Solution:
+    @staticmethod
+    def valid_palindrome(s: str) -> bool:
+        
+        return False
+
+
+if __name__ == '__main__':
+    Solution.valid_palindrome(s="aba")
+    Solution.valid_palindrome(s="abca")
+    Solution.valid_palindrome(s="abc")
