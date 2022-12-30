0b35165503b7108570010f3526b81cc780da3008
Nur <mnjnurrumen@gmail.com>
2022-12-30 11:08:20 +0600

    modify insertion sort

--- DSA90Days/Arrays/Day3/insertion_sort.py
+++ DSA90Days/Arrays/Day3/insertion_sort.py
@@ -1,12 +1,19 @@
+"""
+Insertion Sort tutorial:
+https://www.youtube.com/watch?v=gbJzL6IJig0
+https://www.youtube.com/watch?v=oTICKmJhLXI
+https://www.youtube.com/watch?v=enhpoXDWPcQ
+"""
 from typing import List
 
 
 def insertion_sort(input_arr: List) -> List:
     for i in range(1, len(input_arr)):
-        for j in range(i, 0, -1):
-            point_element = input_arr[i]
-            if input_arr[j-1] > point_element:
-                input_arr[i] = input_arr[j-1]
+        point_element = input_arr[i]
+        for j in range(i-1, -1, -1):
+            if input_arr[j] > point_element:
+                input_arr[j+1] = input_arr[j]
+                input_arr[j] = point_element
 
     return input_arr
 
